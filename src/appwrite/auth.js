4593c5fb78a9f24e6a24ccfa33eaf8dbbf870332
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-08 01:57:36 +0530

    Refactor authentication service and update App component

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -37,7 +37,7 @@ export class AuthService {
 
   async login({ email, password }) {
     try {
-      return await this.account.createSession(email, password);
+      return await this.account.createEmailPasswordSession(email, password);
     } catch (error) {
       throw error;
     }
